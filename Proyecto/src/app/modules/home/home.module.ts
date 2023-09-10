e5639c79e6990d7b28daf225b37a51a2aa2953d2
NatividadArrazolaCan <7491@itescam.edu.mx>
2023-09-10 16:48:04 -0600

    proyecto

--- Proyecto/src/app/modules/home/home.module.ts
+++ Proyecto/src/app/modules/home/home.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from '../home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule
+  ],
+  exports: [
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class HomeModule { }
