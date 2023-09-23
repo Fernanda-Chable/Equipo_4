6632fe19285e2a08a6fc3fbc16e0f0865f8bd31f
Estefanny-Tucuch <143752773+Estefanny-Tucuch@users.noreply.github.com>
2023-09-22 18:02:17 -0600

    nuevo

---
+++ Ejercicios/7979_EstefannyDelCarmenTucuchUcan/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './modules/home/home.component';
+import { HomeModule } from './modules/home/home.module';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './modules/home/calculadora/calculadora.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
