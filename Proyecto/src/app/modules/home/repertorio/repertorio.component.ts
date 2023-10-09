69d1b7a899cdf90bb8fc507a60f49404290ebddb
Fernanda-Chable <80001938+Fernanda-Chable@users.noreply.github.com>
2023-10-08 18:26:49 -0600

    nuevo

--- Proyecto/src/app/modules/home/repertorio/repertorio.component.ts
+++ Proyecto/src/app/modules/home/repertorio/repertorio.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./repertorio.component.scss']
 })
 export class RepertorioComponent {
-
+  isListExpanded: boolean = true;
+  isListExpanded1: boolean = true;
+  isListExpanded2: boolean = false;
+  isListExpanded3: boolean = false;
+  
+  toggleList1() {
+    this.isListExpanded = !this.isListExpanded;
+  }
+  
+  toggleList2() {
+    this.isListExpanded1 = !this.isListExpanded1;
+  }
+  toggleList3() {
+    this.isListExpanded2 = !this.isListExpanded2;
+  }
+  toggleList4() {
+    this.isListExpanded3  = !this.isListExpanded3 ;
+  }
+   
 }
