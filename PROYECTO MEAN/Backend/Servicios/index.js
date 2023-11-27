9623a820dacd989fe2b2efa3687880a8ce04df71
Fernanda-Chable <80001938+Fernanda-Chable@users.noreply.github.com>
2023-11-26 19:31:51 -0600

    dashboard

--- PROYECTO MEAN/Backend/Servicios/index.js
+++ PROYECTO MEAN/Backend/Servicios/index.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const conectarDB = require('./config/db');
 const cors = require("cors");
-const dashboardRouter = require('./routes/dashboard');
+
 
 // Creamos el servidor
 const app = express();
@@ -14,8 +14,7 @@ app.use(cors())
 app.use(express.json());
 app.use('/api/reservas', require('./routes/reserva'));
 app.use('/api/comentarios', require('./routes/comentario'));
-app.use('/api/dashboard', require('./routes/dashboard'));
-app.use('/api/dashboard', dashboardRouter);
+
 app.listen(4000, () => {
     console.log('El servidor esta corriendo perfectamente')
 })
