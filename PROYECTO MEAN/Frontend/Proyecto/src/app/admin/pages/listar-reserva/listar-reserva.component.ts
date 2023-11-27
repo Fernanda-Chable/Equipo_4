67ff9461bdded023642fc3fd5bd6b289314ebc31
NatividadArrazolaCan <7491@itescam.edu.mx>
2023-11-26 20:03:38 -0600

    Reservas

--- PROYECTO MEAN/Frontend/Proyecto/src/app/admin/pages/listar-reserva/listar-reserva.component.ts
+++ PROYECTO MEAN/Frontend/Proyecto/src/app/admin/pages/listar-reserva/listar-reserva.component.ts
@@ -10,7 +10,34 @@ import { ReservaService } from 'src/app/services/reserva.service';
   templateUrl: './listar-reserva.component.html',
   styleUrls: ['./listar-reserva.component.scss']
 })
-export class ListarReservaComponent  {
+export class ListarReservaComponent implements OnInit {
 
+  listReservas: Reserva[] = [];
+
+   constructor(private _reservaService: ReservaService,
+        private toastr: ToastrService,) { }
+
+  ngOnInit(): void {
+    this.obtenerReservas();
+  }
+
+
+  obtenerReservas() {
+    this._reservaService.getReservas().subscribe(data => {
+      console.log(data);
+      this.listReservas = data;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  eliminarReserva(id: any) {
+    this._reservaService.eliminarReserva(id).subscribe(data => {
+      this.toastr.error('La reserva fue eliminado con exito' ,'Reserva Eliminada');
+      this.obtenerReservas();
+    }, error => {
+      console.log(error);
+    })
+  }
 
 }
